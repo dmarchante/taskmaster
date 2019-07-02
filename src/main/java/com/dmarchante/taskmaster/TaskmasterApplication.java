a56b1cdeb871ff77c3d1b80896fd56eb388c9d89
David Marchante <cdmarchante@gmail.com>
2019-07-02 13:17:51 -0700

    complete initial set-up

---
+++ src/main/java/com/dmarchante/taskmaster/TaskmasterApplication.java
@@ -0,0 +1,11 @@
+package com.dmarchante.taskmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmasterApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmasterApplication.class, args);
+	}
+}
